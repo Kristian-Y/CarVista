fa82a385b54e8057f99f25f53d2a9a768bbc4f61
Kristian <krisiyotsov@gmail.com>
2024-07-17 00:19:37 +0300

    .

---
+++ frontend/carvista/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <main>
+        <Nav :isLogin="isLogin"></Nav>
+        <router-view></router-view> <!-- Where routed views are rendered -->
+    </main>
+    <Footer></Footer>
+
+</template> 
+
+<script>
+  import Nav from './components/Nav.vue'
+  import Footer from './components/Footer.vue'
+
+  export default {
+   components: {
+    Nav
+   },
+   data() {
+      return {
+        isLogin: true,
+      }
+   }
+  };
+</script>
+  
+<style></style>
+  
+<script setup>
+  import Nav from './components/Nav.vue'
+
+
+</script>
+  
+<style>
+  html, body {
+    margin: 0;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    height: 100vh; /* Use full viewport height */
+  }
+</style>
---
+++ frontend/carvista/src/components/Nav.vue
@@ -0,0 +1,50 @@
+<template>
+    <nav>
+        <img :src="logo" alt="logo">
+        <a href="" v-if="!isLogin">Test</a>
+        <div class="isLoginBtns" v-if="isLogin">
+            <a href="" class="bars"><i class="fa fa-bars" aria-hidden="true"></i></a>
+            <a href=""><img :src="noProfile" alt=""></a>
+        </div>
+    </nav>
+</template>
+
+<script>
+    import logoImg from '@/assets/imgs/CarVistaLogo.png'; // '@' is an alias to /src in Vue CLI\
+    import noProfile from '@/assets/components/noProfile.png';
+    //import { EventBus } from '@/event-bus.js';
+
+    export default {
+        props: ['isLogin'],
+        data() {
+            return {
+                logo: logoImg,
+                noProfile: noProfile,
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+ 
+<style scoped>
+nav {
+    background-color: black;
+    height: 4vh;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 10px;
+    padding-right: 10px;
+}
+    
+img {
+    height: 2vh;
+}
+
+.bars {
+    color: white;
+}
+
+</style> 
